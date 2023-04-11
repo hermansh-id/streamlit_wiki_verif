f0013ea9ba1a6607f886802e92e6d43cddb74897
hermansh-id <hermansh.id@gmail.com>
2023-04-11 23:01:03 +0700

    fixx when job is done

--- simulation.py
+++ simulation.py
@@ -19,16 +19,19 @@ csv_fold = [f for f in os.listdir(CSV_DATA_FOLDER) if f.endswith('.csv')]
 hasil = [f for f in os.listdir(CSV_HASIL_FOLDER) if f.endswith('.csv')]
 jelek = [f for f in os.listdir(CSV_JELEK) if f.endswith('.csv')]
 csv_files = [x for x in csv_fold if x not in hasil and x not in jelek]
-csv_x = random.choice(csv_files)
+if(len(csv_files) > 0):
+    csv_x = random.choice(csv_files)
 
-csv_path = os.path.join(CSV_DATA_FOLDER, csv_x)
-df = pd.read_csv(csv_path)
+    csv_path = os.path.join(CSV_DATA_FOLDER, csv_x)
+    df = pd.read_csv(csv_path)
 
-st.write("Data: "+str(len(csv_fold) - len(csv_files)) + "/" + str(len(csv_fold)))
-st.dataframe(df)
+    st.write("Data: "+str(len(csv_fold) - len(csv_files)) + "/" + str(len(csv_fold)))
+    st.dataframe(df)
 
-save = st.button('Save')
-no_save = st.button("No save")
+    save = st.button('Save')
+    no_save = st.button("No save")
+else:
+    st.write("Tidak ada yang perlu di kurasi lagi, terimakasih!")
 if(st.button("ZIP")):
     shutil.make_archive("hasil", "zip", CSV_HASIL_FOLDER)
 
@@ -49,7 +52,6 @@ if save:
     csv_files.remove(csv_x)
     df.to_csv(os.path.join("hasil", csv_x))
     csv_x = random.choice(csv_files)
-
 
 if no_save:
     csv_files.remove(csv_x)
